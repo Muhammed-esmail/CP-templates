fbc738f91d066fa56b24c0e8d19321204eab35d8
Muhammed Esmail <muhammedesmail101@gmail.com>
2025-07-25 13:32:22 +0300

    dsu implementation

---
+++ Final Templates/DSU.cpp
@@ -0,0 +1,29 @@
+struct DSU {
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        sz.resize(n + 1, 1);
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int x) {
+        if(x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    bool same(int a, int b) {
+        return find(a) == find(b);
+    }
+
+    void connect(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return;
+        if (sz[a] > sz[b])
+            swap(a, b);
+        parent[a] = b;
+        sz[b] += sz[a];
+    }
+};
