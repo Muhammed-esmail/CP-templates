f81d4596b72fc51ea034fd4f47492b1310b3cfd9
Muhammed Esmail <muhammedesmail101@gmail.com>
2025-07-30 01:35:49 +0300

    Sieve_and_SPF

--- Sieve_and_SPF.cpp
+++ Sieve_and_SPF.cpp
@@ -2,13 +2,14 @@ vector<bool> isprime(N, 1);
 vector<int> spf(N);
 
 void sieve() {
+    iota(all(spf), 0);
     isprime[0] = isprime[1] = 0;
     for (int i = 2; i < N; i++) {
         if (!isprime[i])
             continue;
         spf[i] = i;
         for (int j = i * i; j < N; j += i) {
-            spf[j] = i;
+            spf[j] = min(spf[j], i);
             isprime[j] = 0;
         }
     }
